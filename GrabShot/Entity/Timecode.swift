2d63c138d6ade6f6935a31cd0a4c670be6015f36
DenDmitriev <dv.denstr@gmail.com>
2023-12-14 14:02:04 +0300

    refactoring timecode

--- GrabShot/Entity/Timecode.swift
+++ GrabShot/Entity/Timecode.swift
@@ -15,60 +15,60 @@ struct Timecode: CustomStringConvertible {
     ///
     /// Example: 01:25:59:12 - 1 hour 25 minutes 59 seconds 12 frames
     ///
-    var components: (hours: UInt8, minutes: UInt8, seconds: UInt8, frames: UInt8) {
+    var components: (hours: Int, minutes: Int, seconds: Int, frames: Int) {
         return (hours: hours, minutes: minutes, seconds: seconds, frames: frames)
     }
     
-    var hours: UInt8 {
+    var hours: Int {
         didSet {
             if hours >= 100 {
                 hours = 99
             }
         }
     }
-    var minutes: UInt8 {
+    var minutes: Int {
         willSet {
             if newValue >= 60 {
                 let hours = newValue / 60
-                self.hours += UInt8(hours)
+                self.hours += Int(hours)
             }
         }
         
         didSet {
             if minutes >= 60 {
                 let partMinutes = (Double(minutes) / 60.0).truncatingRemainder(dividingBy: 1)
-                minutes = UInt8(partMinutes * 60)
+                minutes = Int(partMinutes * 60)
             }
         }
     }
-    var seconds: UInt8 {
+    var seconds: Int {
         willSet {
             if newValue >= 60 {
                 let minutes = newValue / 60
-                self.minutes += UInt8(minutes)
+                self.minutes += Int(minutes)
             }
         }
         
         didSet {
             if seconds >= 60 {
                 let partSecond = (Double(seconds) / 60.0).truncatingRemainder(dividingBy: 1)
-                seconds = UInt8(partSecond * 60)
+                seconds = Int(partSecond * 60)
             }
         }
     }
-    var frames: UInt8 {
+    var frames: Int {
         willSet {
             if newValue >= Int(frameRate.value.rounded(.down)) {
                 let seconds = Double(newValue) / frameRate.value
-                self.seconds += UInt8(seconds)
+                self.seconds += Int(seconds)
             }
         }
         
         didSet {
-            let maxFrame = UInt8(frameRate.value.rounded(.down))
+            let maxFrame = Int(frameRate.value.rounded(.down))
             if frames >= maxFrame {
                 let partSecond = (Double(frames) / Double(maxFrame)).truncatingRemainder(dividingBy: 1)
-                frames = UInt8(partSecond * frameRate.value)
+                frames = Int(partSecond * frameRate.value)
             }
         }
     }
@@ -117,17 +117,17 @@ struct Timecode: CustomStringConvertible {
         }
     }
     
-    init(hours: UInt8 = .zero, minutes: UInt8 = .zero, seconds: UInt8 = .zero, frames: UInt8 = .zero, frameRate: FrameRate) {
-        let hoursChecked: UInt8 = 0...23 ~= hours ? hours : .zero
+    init(hours: Int = .zero, minutes: Int = .zero, seconds: Int = .zero, frames: Int = .zero, frameRate: FrameRate) {
+        let hoursChecked: Int = 0...23 ~= hours ? hours : .zero
         self.hours = hoursChecked
         
-        let minutesChecked: UInt8 = 0...59 ~= minutes ? minutes : .zero
+        let minutesChecked: Int = 0...59 ~= minutes ? minutes : .zero
         self.minutes = minutesChecked
         
-        let secondsChecked: UInt8 = 0...59 ~= seconds ? seconds : .zero
+        let secondsChecked: Int = 0...59 ~= seconds ? seconds : .zero
         self.seconds = secondsChecked
         
-        let framesChecked: UInt8 = 0...UInt8(frameRate.value.rounded(.down)) ~= frames ? frames : .zero
+        let framesChecked: Int = 0...Int(frameRate.value.rounded(.down)) ~= frames ? frames : .zero
         self.frames = framesChecked
         
         self.frameRate = frameRate
@@ -143,7 +143,7 @@ struct Timecode: CustomStringConvertible {
         
         let missingCount = 4 - inputComponents.count
         if missingCount > 0 {
-            for _ in 0...missingCount {
+            for _ in 1...missingCount {
                 inputComponents.insert(.zero, at: 0)
             }
         }
@@ -151,17 +151,17 @@ struct Timecode: CustomStringConvertible {
         for index in 0...3 {
             switch index {
             case 0:
-                let hours = inputComponents[0] / 24.0
-                timecode.hours = UInt8(hours)
+                let hours = inputComponents[0]
+                timecode.hours = Int(hours)
             case 1:
-                var minutes = inputComponents[1] / 60.0
-                timecode.minutes = UInt8(minutes)
+                var minutes = inputComponents[1]
+                timecode.minutes = Int(minutes)
             case 2:
-                var seconds = inputComponents[2] / 60.0
-                timecode.seconds = UInt8(seconds)
+                var seconds = inputComponents[2]
+                timecode.seconds = Int(seconds)
             case 3:
-                var frames = inputComponents[3] / frameRate.value
-                timecode.frames = UInt8(frames)
+                var frames = inputComponents[3]
+                timecode.frames = Int(frames)
             default:
                 break
             }
@@ -178,9 +178,10 @@ struct Timecode: CustomStringConvertible {
         let seconds = secondsRounded % 60
         let frames = Double(seconds).truncatingRemainder(dividingBy: 1) * frameRate.value
         
-        return Self.init(hours: UInt8(hours), minutes: UInt8(minutes), seconds: UInt8(seconds), frames: UInt8(frames), frameRate: frameRate)
+        return Self.init(hours: Int(hours), minutes: Int(minutes), seconds: Int(seconds), frames: Int(frames), frameRate: frameRate)
     }
-    static private func formatted(component: UInt8) -> String {
+    
+    static private func formatted(component: Int) -> String {
         if String(component).count <= 1 {
             return "0\(component)"
         } else {

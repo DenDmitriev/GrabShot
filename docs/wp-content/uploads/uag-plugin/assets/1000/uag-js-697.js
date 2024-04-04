9322a3b58c7ec400772a5d6fdf3473ff6638fc99
DenDmitriev <dv.denstr@gmail.com>
2024-04-04 17:37:15 +0300

    add site

---
+++ docs/wp-content/uploads/uag-plugin/assets/1000/uag-js-697.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false,"makeCollapsible":false}, '.uagb-block-0e4755ef' );
+} );
+window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false,"makeCollapsible":false}, '.uagb-block-95f74e1b' );
+} );
+ });

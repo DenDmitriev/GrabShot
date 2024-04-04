bd1116de48a7387a2788183e55f790780b86206a
DenDmitriev <dv.denstr@gmail.com>
2024-04-04 17:35:15 +0300

    delete site

--- docs/wp-content/uploads/uag-plugin/assets/1000/uag-js-567.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	var swiper = new Swiper( ".uagb-block-4d155ec8 .uagb-swiper",
-		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-4d155ec8 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-4d155ec8 .swiper-button-next","prevEl":".uagb-block-4d155ec8 .swiper-button-prev"}}	);
-});
-
- });
